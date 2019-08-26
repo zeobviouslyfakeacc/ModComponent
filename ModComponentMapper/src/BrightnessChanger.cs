3fdd6e3e4516856dace27d0137cfa64c3a51d06d
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2019-08-26 19:10:59 +0200

    ColorGradingRenderer is now internal in its assembly

--- ModComponentMapper/src/BrightnessChanger.cs
+++ ModComponentMapper/src/BrightnessChanger.cs
@@ -1,11 +1,19 @@
-﻿namespace ModComponentMapper
+﻿using System;
+using System.Reflection;
+using Harmony;
+
+namespace ModComponentMapper
 {
     public class BrightnessChanger
     {
+        private static readonly Assembly UnityAssembly = typeof(UnityEngine.Rendering.PostProcessing.ColorGrading).Assembly;
+        private static readonly Type ColorGradingRenderer = UnityAssembly.GetType("UnityEngine.Rendering.PostProcessing.ColorGradingRenderer");
+        private static readonly FieldInfo BrightnessField = AccessTools.Field(ColorGradingRenderer, "s_Brightness");
+
         public static float Brightness
         {
-            get => UnityEngine.Rendering.PostProcessing.ColorGradingRenderer.s_Brightness;
-            set => UnityEngine.Rendering.PostProcessing.ColorGradingRenderer.s_Brightness = value;
+            get => (float) BrightnessField.GetValue(null);
+            set => BrightnessField.SetValue(null, value);
         }
 
         public static float GetDefault()
